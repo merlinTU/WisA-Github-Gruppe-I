7b0d2bf241e51db39ab0f169c5fe7d664dc885b6
T-M <tim.mergel@tu-dortmund.de>
2025-02-10 12:15:40 +0100

    Fehler in Daten aus Cabin extrahieren behoben, wo es ein Problem mit Einträgen, die nicht Zahlen waren gab: In all(number%%2) : coercing argument of type 'double' to logical. Und den verarbeiteten Datesantz hochgeladen

--- prep_data.R
+++ prep_data.R
@@ -2,20 +2,20 @@
 titanic <- read.csv("~/Downloads/titanic.csv")
 
 # Anreden
-  
-  #Anrede extrahieren
-  titanic$Title <- sub(".*, (.*?)\\..*", "\\1", titanic$Name)
-  
-  
-  # gleiche Anreden durch ein Wort ersetzten
-  titanic$Title <- gsub("Mlle|Ms|Miss", "Miss", titanic$Title)
-  titanic$Title <- gsub("Mme", "Mrs", titanic$Title)
-  titanic$Title <- gsub("Master", "Master", titanic$Title)
 
-  
+#Anrede extrahieren
+titanic$Title <- sub(".*, (.*?)\\..*", "\\1", titanic$Name)
+
+
+# gleiche Anreden durch ein Wort ersetzten
+titanic$Title <- gsub("Mlle|Ms|Miss", "Miss", titanic$Title)
+titanic$Title <- gsub("Mme", "Mrs", titanic$Title)
+titanic$Title <- gsub("Master", "Master", titanic$Title)
+
+
 table(titanic$Title)
 # hat tatsächlich noch komische Einträge, Don: spanischer Ehrentitel, Jonkheer: niederländischer Adelstitel, Rev: irgendwas mit Kirche, Col: Colonel also Militär
-  
+
 
 # Survived, Sex, Embarked und Pclass in Faktoren umwandeln
 
@@ -45,21 +45,26 @@ titanic$Deck <- NA
 
 for(i in 1:nrow(titanic)){
   dat = titanic$Cabin[i]
-  if(dat != ""){
+  if(!is.na(dat) && dat != ""){
     dat = strsplit(dat, " ")[[1]]
     titanic$Deck[i] <- paste(unique(gsub("[^A-Z]", "", dat)), collapse = ",")
     number <- na.omit(as.numeric(gsub("[^0-9 ]", "", dat)))
-    if(all(number %% 2))
-      titanic$Bord[i] <- "S"
-    else
-      titanic$Bord[i] <- "B"
+    
+    # Überprüfung, ob number nicht leer ist
+    if(length(number) > 0){
+      if(all(number %% 2 == 0)) {
+        titanic$Bord[i] <- "S"
+      } else {
+        titanic$Bord[i] <- "B"
+      }
+    }
   }
 }
 
 # Entferne die Spalten "PassengerID", "Name", "Ticket" und "Cabin" aus dem Datensatz
 # (erst zum Schluss)
 titanic <- titanic[, !(names(titanic) %in% c("PassengerID", "Name", "Ticket", "Cabin"))]
-
+titanic <- titanic[, -1]
 # Datensatz speichern
 
 write.csv(titanic, "~/Downloads/titanic_prep.csv", row.names = FALSE)
